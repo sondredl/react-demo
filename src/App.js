046b342c3556da47e70335e55f6152bfb7489e62
Sondre Lind <sondredl@gmailcom>
2023-11-17 23:54:19 +0100

    fix companies tab showing content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import YourComponent from './jsonData';
-import React ,  {useState} from 'react';
+// import companies from './companies.json';
+// import React ,  {useState} from 'react';
 import Tabs from './tabs';
+import React, {useState, useEffect } from 'react';
 
 const App = () => {
   const [currentTab, setCurrentTab] = useState('Companies');
+  const [jsonData, setJsonData] = useState([]);
 
   const handleTabChange = (tab) => {
     setCurrentTab(tab);
   };
+
+  useEffect(() => {
+    fetch('/companies.json')
+    .then((response) => response.json())
+    .then((data) => setJsonData(data))
+    .then((error) => console.error('Error fetching JSON data:' , error));
+  }, []);
 
   return (
     <div>
@@ -18,19 +28,23 @@ const App = () => {
         defaultTab="Companies"
         onTabChange={handleTabChange}
         />
-        {currentTab === 'Companies' && <CompaniesTab />}
+        {currentTab === 'Companies' && <CompaniesTab jsonData={jsonData} />}
         {currentTab === 'OtherTab1' && <OtherTab1 />}
         {currentTab === 'OtherTab2' && <OtherTab2 />}
       </div>
   );
 };
 
-const CompaniesTab = () => {
+const CompaniesTab = ({jsonData}) => {
   // The content of the "Companies" tab
   return (
     <div>
       <h1>Companies</h1>
-      {/* Your existing code for displaying companies */}
+            <ul>
+                {jsonData.map((company, index) => (
+                    <li key={index}>{company} </li>
+                ))}
+            </ul>
     </div>
   );
 };
