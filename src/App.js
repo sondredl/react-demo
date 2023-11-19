ab0e60f8ebce016e3f55c5ec8136fd57c3acaa89
Sondre Lind <sondredl@gmailcom>
2023-11-19 21:03:42 +0100

    adds more content to page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const App = () => {
   const [currentTab, setCurrentTab] = useState('Companies');
   const [jsonData, setJsonData] = useState([]);
   const [searchWords, setSearchWords] = useState([]);
+  const [webPages, setWebPages] = useState([]);
+  const [htmlTags, setHtmlTags] = useState([]);
 
   const handleTabChange = (tab) => {
     setCurrentTab(tab);
@@ -26,15 +28,31 @@ const App = () => {
     .catch((error) => console.error('Error fetching SearcWords data:' , error));
   }, []);
 
+  useEffect(() => {
+    fetch('/webPages.json')
+    .then((response) => response.json())
+    .then((data) => setWebPages(data))
+    .catch((error) => console.error('Error fetching SearcWords data:' , error));
+  }, []);
+
+  useEffect(() => {
+    fetch('/htmlTags.json')
+    .then((response) => response.json())
+    .then((data) => setHtmlTags(data))
+    .catch((error) => console.error('Error fetching SearcWords data:' , error));
+  }, []);
+
   return (
     <div>
       <Tabs
-        tabs={['Companies', 'SearchWords']} 
+        tabs={['Companies', 'SearchWords', 'WebPages', 'HtmlTags']} 
         defaultTab="Companies"
         onTabChange={handleTabChange}
         />
         {currentTab === 'Companies' && <CompaniesTab jsonData={jsonData} />}
         {currentTab === 'SearchWords' && <SearchWordsTab searchWords={searchWords}/>}
+        {currentTab === 'WebPages' && <WebPagesTab webPages={webPages}/>}
+        {currentTab === 'HtmlTags' && <HtmlTagsTab htmlTags={htmlTags}/>}
       </div>
   );
 };
@@ -65,5 +83,34 @@ const SearchWordsTab = ({searchWords}) => {
   );
 };
 
+const WebPagesTab = ({webPages}) => {
+  return (
+    <div>
+      <h1>WebPages</h1>
+      <ul>
+        {webPages.map((page, index) => (
+          <li key={index}>
+            <a href={page.url} target="_blank" rel="noopener noreferrer">
+              {page.name} 
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const HtmlTagsTab = ({htmlTags}) => {
+  return (
+    <div>
+      <h1>HtmlTags</h1>
+      <ul>
+        {htmlTags.map((tag, index) => (
+          <li key={index}> {tag} </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
