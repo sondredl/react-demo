99d3bd0a631072a6acc7997da3c84588123c42de
Sondre Lind <sondredl@gmailcom>
2023-11-19 20:46:31 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-// import './companies.json';
-// import './searchWords.json';
-// import YourComponent from './jsonData';
-// import SerchWordsComponent from './searchWords';
-// import companies from './companies.json';
-// import React ,  {useState} from 'react';
 import Tabs from './tabs';
 import React, {useState, useEffect } from 'react';
 
@@ -25,53 +19,27 @@ const App = () => {
     .then((error) => console.error('Error fetching JSON data:' , error));
   }, []);
 
-  // useEffect(() => {
-  //   fetch('/searchWords.json')
-  //   .then((response) => {
-  //     console.log('Response: ', response);
-  //     return response.json();
-  //   })
-  //   .then((data) => {
-  //     console.log('Data: ', data)
-  //     setSearchWords(data);
-  //   })
-  //   .catch((error) => console.error('Error fetching SearcWords data:' , error));
-  // }, []);
   useEffect(() => {
-  // Fetch searchWords data
-  fetch('/searchWords.json', {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
+    fetch('/searchWords.json')
     .then((response) => response.json())
     .then((data) => setSearchWords(data))
-    .catch((error) => console.error('Error fetching searchWords data:', error));
-}, []);
-  // useEffect(() => {
-  //   fetch('/searchWords.json')
-  //   .then((response) => response.json())
-  //   .then((data) => setSearchWords(data))
-  //   .catch((error) => console.error('Error fetching SearcWords data:' , error));
-  // }, []);
+    .catch((error) => console.error('Error fetching SearcWords data:' , error));
+  }, []);
 
   return (
     <div>
       <Tabs
-        tabs={['Companies', 'SearchWords', 'OtherTab2']} 
+        tabs={['Companies', 'SearchWords']} 
         defaultTab="Companies"
         onTabChange={handleTabChange}
         />
         {currentTab === 'Companies' && <CompaniesTab jsonData={jsonData} />}
         {currentTab === 'SearchWords' && <SearchWordsTab searchWords={searchWords}/>}
-        {currentTab === 'OtherTab2' && <OtherTab2 />}
       </div>
   );
 };
 
 const CompaniesTab = ({jsonData}) => {
-  // The content of the "Companies" tab
   return (
     <div>
       <h1>Companies</h1>
@@ -85,7 +53,6 @@ const CompaniesTab = ({jsonData}) => {
 };
 
 const SearchWordsTab = ({searchWords}) => {
-  // The content of the "OtherTab1" tab
   return (
     <div>
       <h1>SearchWords</h1>
@@ -98,9 +65,5 @@ const SearchWordsTab = ({searchWords}) => {
   );
 };
 
-const OtherTab2 = () => {
-  // The content of the "OtherTab2" tab
-  return <div>Content for OtherTab2</div>;
-};
 
 export default App;
--- src/jsonData.js
+++ src/jsonData.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect } from 'react';
-import companies from './companies.json';
 
 const YourComponent = () => {
     const [companies, setCompanies] = useState([]);
